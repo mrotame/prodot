ca401b5a7e86a14b2a047e96bb9f6fa3584f7f4f
Matheus Menezes Almeida <mrotame@gmail.com>
2023-02-03 09:53:08 -0300

    abstracted dot_object into multiple classes

--- prodot/dot_object.py
+++ prodot/dot_object.py
@@ -1,28 +1,5 @@
 import typing as t
+from .dict_class import DictClass
 
-class DotObject:
-    main_object: t.Dict
-
-    def __init__(self, main_object: t.Union[t.Dict,None]= dict()):
-        self.main_object = main_object
-
-    def __repr__(self) -> t.Dict:
-        return str(self.main_object)
-
-    def __getattr__(self, name: str) -> t.Any:
-        if name in self.main_object:
-            return DotObject(self.main_object[name])
-        else:
-            raise Exception(f"Key doesn't exist in DotObject {self.true_repr()}")
-
-    def __setattr__(self, name: str, value: t.Any) -> None:
-        if name == 'main_object':
-            self.__dict__[name] = value
-        else:
-            self.main_object[name] = value
-        
-    def true_repr(self):
-        type_ = type(self)
-        module = type_.__module__
-        qualname = type_.__qualname__
-        return f"<{module}.{qualname} object at {hex(id(self))}>"
+class DotObject(DictClass):
+    pass
